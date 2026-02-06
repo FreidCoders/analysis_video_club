b89a61d88b7622c3f8ef46d3735e2c8dc6a3cd4e
freidcoders <freidagnontcheme@gmail.com>
2026-02-06 09:00:54 +0100

    Update configuration readme & subfolders in scripts

--- Readme.md
+++ Readme.md
@@ -1,2 +1,8 @@
 # Analyse video Club
 
+## Configuration de base d'un projet
+
+-   Ajout du package ***renv***
+-   Ajout du dossier **data** (jeu de donnes brutes & traités)
+-   Ajout du dossier **scripts** (fichier R d'analyse & de manipulation)
+-   Ajout du dossier **outputs** (fichier de sortie des graphiques)
--- scripts/scripts.R
+++ scripts/scripts.R
@@ -5,7 +5,7 @@ library(plotly)
 #Charger les jeux de données
 
 clients <- read_csv("data/raw/client.csv")
-category <- read_csv("data/raw/category_sales.csv")
+categories <- read_csv("data/raw/category_sales.csv")
 sales_by_store <- read_csv("data/raw/sales_by_store.csv")
 category_by_shop <- read_csv("data/raw/categories_by_boutik.csv")
 top_ten <- read_csv("data/raw/Topten.csv")
@@ -14,10 +14,10 @@ top_ten <- read_csv("data/raw/Topten.csv")
 
 #1-traitement & visualisation de catégorie
 
-ggplot(category,aes(category,total_sales,fill = category))+ 
+ggplot(categories,aes(category,total_sales,fill = category))+ 
   geom_bar(stat = "identity")+
   labs(title = "Visualization des ventes par catégories")+
   coord_flip()
 
-ggsave("outputs/plots/category_sales.png")
+#ggsave("outputs/plots/category_sales.png")
 #traitement & visualisation de clients
